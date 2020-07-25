b9204595508ca744409d6fae6d17f49ee99b5dd8
Jakexsc <649199985@qq.com>
2020-07-26 01:10:30 +0800

    IntStream.rangeClosed 勾股定理实践

--- src/com/xsc/study/stream/TestStream.java
+++ src/com/xsc/study/stream/TestStream.java
@@ -192,6 +192,15 @@ public class TestStream {
                 .filter(n -> n % 2 == 0)
                 .count();
         System.out.println(count);
-    }
 
+        // 求出1-100 能组成勾股定理的组合
+        IntStream.rangeClosed(1, 100)
+                .boxed()
+                .flatMap(a ->
+                        IntStream.rangeClosed(a, 100).filter(b -> Math.sqrt(a * a + b * b) % 1 == 0)
+                                .mapToObj(b -> new int[]{a, b, (int) Math.sqrt(a * a + b * b)}).filter(t -> t[2] % 1 == 0)
+                ).limit(5).forEach(t -> {
+            System.out.println(t[0] + "," + t[1] + "," + t[2]);
+        });
+    }
 }
